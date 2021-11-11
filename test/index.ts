9f76fbf2bd621c2d598c67579acbc388ecc0bb41
andreiashu <andreiashu@gmail.com>
2021-11-11 19:55:41 +0700

    💾 add liquidity

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,8 @@
 import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
-import { BigNumber } from 'ethers';
+import { BigNumber, Contract } from 'ethers';
 import { formatUnits, namehash, parseUnits } from "ethers/lib/utils";
+import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 
 import UniswapV2Factory from "../buildV2/UniswapV2Factory.json";
 
@@ -10,6 +11,10 @@ const { deployContract } = waffle;
 const { MaxUint256 } = ethers.constants;
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const overrides = {
+  gasLimit: 9999999
+}
 
 describe("Uniswap Factory", function () {
   let owner;
@@ -37,26 +42,32 @@ describe("Uniswap Factory", function () {
     console.log(`\ttoken0: ${token0.address}`);
     console.log(`\ttoken1: ${token1.address}`);
 
-    // const UniFactory = await ethers.getContractFactory("UniswapV2Factory");
-    // const uniFactory = await UniFactory.deploy(ZERO_ADDRESS);
-    // await uniFactory.deployed();
-    // const uniFactory = await deployContract(owner, UniswapV2Factory, [owner.address]);
-
-
-
     const uniFactory = await deployContract(owner, UniswapV2Factory, [ZERO_ADDRESS]);
     expect(await uniFactory.feeToSetter()).to.equal(ZERO_ADDRESS);
 
     const RouterV2Factory = await ethers.getContractFactory("UniswapV2Router02");
     const router = await RouterV2Factory.deploy(uniFactory.address, WETH.address);
     await router.deployed();
-    const pair = await uniFactory.createPair(token0.address, token1.address);
-    console.log(pair);
+    await uniFactory.createPair(token0.address, token1.address);
     await token0.approve(router.address, MaxUint256);
     await token1.approve(router.address, MaxUint256);
 
     const pairAddress = await uniFactory.getPair(token0.address, token1.address);
-    console.log('pairAddress', pairAddress);
+    console.log('\tpairAddress', pairAddress);
+    const pair = new Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi));
+    await router.addLiquidity(
+      token0.address,
+      token1.address,
+      BigNumber.from(10000),
+      BigNumber.from(10000),
+      0,
+      0,
+      ownerAddress,
+      MaxUint256,
+      overrides
+    )
+    const path = [token0.address, token1.address]
+    expect(await router.getAmountsIn(BigNumber.from(1), path)).to.deep.eq([BigNumber.from(2), BigNumber.from(1)])
 
     // console.log(`pair address: ${pairAddress}`);
   });
